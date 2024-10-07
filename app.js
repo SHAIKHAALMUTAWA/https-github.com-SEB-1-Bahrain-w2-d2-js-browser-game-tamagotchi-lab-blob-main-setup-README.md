9fb2567220426450cc16ee41c100ed1d6442890e
shaikhaalmutawa <shaikha23798@gmail.com>
2024-10-07 10:21:36 +0300

    hw

---
+++ app.js
@@ -0,0 +1,36 @@
+/*-------------------------------- Constants --------------------------------*/
+
+const state = {
+    boredom: 0,
+    hunger: 0,
+    sleepiness: 0,
+};
+
+/*---------------------------- Variables (state) ----------------------------*/
+
+let timer;
+let gameOver;
+
+/*------------------------ Cached Element References ------------------------*/
+
+const boredomStatEl = document.querySelector('#boredom-stat');
+const hungerstatEl1 = document.querySelector('#hunger-stat');
+const sleepinessstatEl = document.querySelector('#sleepiness-stat');
+
+const gameMessageEl = document.querySelector('#message');
+
+const resetBtnEl = document.querySelector('#resetBtn');
+
+console.log (boredomStatEl);
+
+/*-------------------------------- Functions --------------------------------*/
+
+function getTimer (){
+timer = 
+};
+
+function getGameOver (){
+
+};
+
+/*----------------------------- Event Listeners -----------------------------*/
